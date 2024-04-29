ab7d954d3edc061450dc3e6fbbe020678defcb21
Juan Manuel Ambriz Núñez <98995634+jman0306@users.noreply.github.com>
2024-04-29 15:31:21 -0600

    Create EVistaFiesta.java

---
+++ ProyectoFinal/EVistaFiesta.java
@@ -0,0 +1,19 @@
+/*
+ * Juan Manuel Ambriz Nu�ez  195554
+ * 25/11/2020
+ * Clase para trabajar con el estacionamiento a traves de una ventana 
+ */
+public class EVistaFiesta {
+
+	public static void main(String[] args) {
+		//VistaEstacionamiento miVentana;
+		ControladorVistaFiesta miVentana;
+		
+		miVentana= new ControladorVistaFiesta();
+		miVentana.pack();
+		miVentana.setVisible(true);
+		
+
+	}
+
+}
